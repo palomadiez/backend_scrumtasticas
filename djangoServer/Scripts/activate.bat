1920f06ec4823c725efcdea939142f6ea8934adb
mariaascaniom <mariaascaniom@gmail.com>
2025-04-06 16:45:21 +0200

    entorno actualizado

--- djangoServer/Scripts/activate.bat
+++ djangoServer/Scripts/activate.bat
@@ -8,15 +8,15 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\pdama\Documents\PALOMA ICAI\3ºiMAT\CUATRI_2\Desarrollo de Aplicaciones y Servicios\t3_backend\djangoServer
+set "VIRTUAL_ENV=C:\Users\maria\OneDrive\Documentos\3º IMAT\2ºCUATRI\DAS\final project\backend_scrumtasticas\djangoServer"
 
 if not defined PROMPT set PROMPT=$P$G
 
 if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
-set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(djangoServer) %PROMPT%
+set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+set "PROMPT=(djangoServer) %PROMPT%"
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -24,8 +24,8 @@ set PYTHONHOME=
 if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
-set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(djangoServer) 
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+set "VIRTUAL_ENV_PROMPT=djangoServer"
 
 :END
 if defined _OLD_CODEPAGE (
